40b7ebefa269ffefa02c804bb384018898d01c47
lyandeveloper <tutors3d@gmail.com>
2022-06-14 15:14:06 -0300

    feat: should return 500 if CreateAccount throws

--- src/presentation/createAccount/createAccount.ts
+++ src/presentation/createAccount/createAccount.ts
@@ -1,23 +1,41 @@
+import { CreateAccount } from "../../core/usecases/createAccount/createAccount";
 import { MissingParamError } from "../errors/MissingParamError";
 import { badRequest } from "../helpers/http-helper";
 import { Controller } from "../http/controller";
 import { httpRequest, httpResponse } from "../http/http";
 
 export class CreateAccountController implements Controller {
-  constructor() {}
+  constructor(private readonly createAccount: CreateAccount) {}
 
   async handle(request: httpRequest): Promise<httpResponse> {
-    const requiredFields = ['name', 'email', 'password'];
 
-    for(let field of requiredFields) {
-      if(!request.body[field]) {
-        return badRequest(new MissingParamError(field))
-      } 
-    }
+    try {
+      const requiredFields = ['name', 'email', 'password'];
 
-    return {
-      status: 201,
-      body: 'success'
+      for(let field of requiredFields) {
+        if(!request.body[field]) {
+          return badRequest(new MissingParamError(field))
+        } 
+      }
+
+      const { name, email, password } = request.body;
+      const account = await this.createAccount.create({
+        name,
+        email,
+        password
+      })
+
+      return {
+        status: 201,
+        body: account
+      }
+    } catch(error) {
+      console.error(error);
+      
+      return {
+        status: 500,
+        body: 'serverError'
+      }
     }
   }
 } 
